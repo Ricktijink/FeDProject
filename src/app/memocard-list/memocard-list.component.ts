ca90e4cd9d1d059f9a6c9e6b2c4ce30cbf4abf78
Rick Tijink <ricktijink@hotmail.com>
2018-09-25 11:28:25 +0200

    Minor changes to css of the form and memocard detail buttons

--- src/app/memocard-list/memocard-list.component.ts
+++ src/app/memocard-list/memocard-list.component.ts
@@ -60,7 +60,7 @@ export class MemocardListComponent implements OnInit {
       this.ngOnInit() },
       error => console.log(error)
     );   
-    alert("Memo is deleted");
+    alert("Memo " + id + " is deleted");
   }
 
   details(id) {
