93e6f2f329155965456b3199a87be1e19e8f0a04
Rick Tijink <ricktijink@hotmail.com>
2018-09-25 09:15:42 +0200

    Subject numbers works but gives errors

--- src/app/articles.service.ts
+++ src/app/articles.service.ts
@@ -14,14 +14,14 @@ export class ArticlesService {
 
   // Get items from database
   getArticles() {
-    return  this.http.get('http://localhost:3000/')
+    return  this.http.get('http://localhost:3000/memo')
     .pipe(catchError((err: any, caught: Observable<{}>) => void console.log(err))
     );
   }
 
   // items of a specified subject from database
   getSubject(subject) {
-    return this.http.get('http://localhost:3000/' + subject)
+    return this.http.get('http://localhost:3000/memo/' + subject)
     .pipe(catchError((err: any, caught: Observable<{}>) => void console.log(err))
     );
   }
@@ -35,15 +35,50 @@ export class ArticlesService {
 
   // Delete items from database
   deleteArticle(id) {
-    return this.http.delete('http://localhost:3000/' + id )
+    return this.http.delete('http://localhost:3000/memo/' + id )
     .pipe(catchError((err: any, caught: Observable<{}>) => void console.log(err))
     );
   }
 
   // Get information of one item from database
   articleDetails(id) {
-    return this.http.get('http://localhost:3000/' + id )
+    return this.http.get('http://localhost:3000/memo/' + id )
     .pipe(catchError((err: any, caught: Observable<{}>) => void console.log(err))
     );
   }
+
+    // TEST counts all memo's in subject
+    countSubjectAll() {
+      return  this.http.get('http://localhost:3000/countSubjectAll')
+      .pipe(catchError((err: any, caught: Observable<{}>) => void console.log(err))
+      );
+    }
+
+    // TEST counts all memo's in subject
+    countSubjectMysql() {
+      return  this.http.get('http://localhost:3000/countSubjectMysql')
+      .pipe(catchError((err: any, caught: Observable<{}>) => void console.log(err))
+      );
+    }
+
+    // TEST counts all memo's in subject
+    countSubjectExpressjs() {
+      return  this.http.get('http://localhost:3000/countSubjectExpressjs')
+      .pipe(catchError((err: any, caught: Observable<{}>) => void console.log(err))
+      );
+    }    
+
+    // TEST counts all memo's in subject
+    countSubjectAngular() {
+      return  this.http.get('http://localhost:3000/countSubjectAngular')
+      .pipe(catchError((err: any, caught: Observable<{}>) => void console.log(err))
+      );
+    }   
+    
+    // TEST counts all memo's in subject
+    countSubjectNodejs() {
+      return  this.http.get('http://localhost:3000/countSubjectNodejs')
+      .pipe(catchError((err: any, caught: Observable<{}>) => void console.log(err))
+      );
+    }   
 }
--- src/app/subject-overview/subject-overview.component.ts
+++ src/app/subject-overview/subject-overview.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { ArticlesService } from '../articles.service';
 
 @Component({
   selector: 'app-subject-overview',
@@ -9,13 +10,43 @@ import { Router, NavigationEnd } from '@angular/router';
 export class SubjectOverviewComponent implements OnInit {
 
   currentUrl: string;
+  countSubjectAll: number;
+  countSubjectMysql: number;
+  countSubjectExpressjs: number;
+  countSubjectAngular: number;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private articlesService: ArticlesService) {
     this.router.events.subscribe((path: NavigationEnd) => {
       if(path.url){
       this.currentUrl = path.url;
+
+      // countSubjectAll
+      articlesService.countSubjectAll().subscribe(json => { 
+        this.countSubjectAll = json.countSubjectAll    // Deze genereert in de compiler van VScode een problem indicatie maar de code is functioneel
+      })
+
+      // countSubjectMysql
+      articlesService.countSubjectMysql().subscribe(json => { 
+        this.countSubjectMysql = json.countSubjectMysql    // Deze genereert in de compiler van VScode een problem indicatie maar de code is functioneel
+      })
+
+      // countSubjectExpressjs
+      articlesService.countSubjectExpressjs().subscribe(json => { 
+        this.countSubjectExpressjs = json.countSubjectExpressjs    // Deze genereert in de compiler van VScode een problem indicatie maar de code is functioneel
+      })
+
+      // countSubjectAngular
+      articlesService.countSubjectAngular().subscribe(json => { 
+        this.countSubjectAngular = json.countSubjectAngular    // Deze genereert in de compiler van VScode een problem indicatie maar de code is functioneel
+      })
+
+      // countSubjectNodejs
+      articlesService.countSubjectNodejs().subscribe(json => { 
+        this.countSubjectNodejs = json.countSubjectNodejs    // Deze genereert in de compiler van VScode een problem indicatie maar de code is functioneel
+      })
+
       }
-      });
+    });
   }
 
   ngOnInit() {}
