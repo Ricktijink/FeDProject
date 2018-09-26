e1a6cdc891a09c12c6e05c4a542592db9528c61a
lbragadossantos <luisasbraga@gmail.com>
2018-09-26 13:30:12 +0200

    bug fixes

--- src/app/subject-overview/subject-overview.component.ts
+++ src/app/subject-overview/subject-overview.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 
+import { ArticlesService } from '../articles.service';
+
+
 @Component({
   selector: 'app-subject-overview',
   templateUrl: './subject-overview.component.html',
@@ -9,8 +12,9 @@ import { Router, NavigationEnd } from '@angular/router';
 export class SubjectOverviewComponent implements OnInit {
 
   currentUrl: string;
+  articlesDT
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private articlesService: ArticlesService) {
     this.router.events.subscribe((path: NavigationEnd) => {
       if(path.url){
       this.currentUrl = path.url;
@@ -18,5 +22,26 @@ export class SubjectOverviewComponent implements OnInit {
       });
   }
 
-  ngOnInit() {}
+  getArticles() {
+    this.articlesService.getArticles().subscribe(articles => this.articlesDT = articles, error => console.log(error))
+  }
+
+  getSubject(subject) {
+    console.log("You have clicked the: \'" + subject + "\' subject")
+    this.articlesService.getSubject(subject).subscribe(
+      articles => this.articlesDT = articles, error => console.log(error)
+    );
+  }
+
+  ngOnInit() {
+    let pathname = window.location.pathname
+    let appId = pathname.split('/')[1]
+
+    if (appId == 'all') {
+      this.getArticles()
+    }
+    else {
+      this.getSubject( appId.toString());
+    }
+  }
 }
