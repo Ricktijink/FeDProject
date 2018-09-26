85d44e7888062e246a17ccd16d859f66d9ff5f28
lbragadossantos <luisasbraga@gmail.com>
2018-09-26 11:56:42 +0200

    changed path ater submit new memo

--- src/app/create-memo-form/create-memo-form.component.ts
+++ src/app/create-memo-form/create-memo-form.component.ts
@@ -12,16 +12,16 @@ import { ArticlesService } from '../articles.service';
 })
 export class CreateMemoFormComponent implements OnInit {
 
-  articles: Articles = { id: 0, subject: '', title: '', description: '', published: new Date(), text: ''}
+  articles: Articles = { id: 0, subject: 'x', title: '', description: '', published: new Date(), text: ''}
   
 
   submit() {
-    // Console log test the input
     console.log(" submit the following " + this.articles.title);
 
-    this.articlesService.sendArticles(this.articles).subscribe()
-    alert("Memo is created");
-    this.router.navigateByUrl('/all');
+    this.articlesService.sendArticles(this.articles).subscribe( () => 
+    window.location.href = '/all')
+    // FIXME: this.router.navigateByUrl('/all'))
+    alert("Memo is created"); 
   }
 
   constructor(private router: Router, private articlesService: ArticlesService) { }
